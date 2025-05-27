3a124a5d7776383c495d118e149233948bab3231
JItendra Bhalothia <write2bhalothia@gmail.com>
2025-05-27 08:41:16 +0200

    refactor: adding variables.tf and outputs.tf files

---
+++ modules/networking/variables.tf
@@ -0,0 +1,11 @@
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "List of availability zones"
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
