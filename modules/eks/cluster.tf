906876221e71fc07e650b0319f717fdfed59c098
JItendra Bhalothia <write2bhalothia@gmail.com>
2025-05-27 08:45:39 +0200

    feat: deploy EKS cluster and managed node group

---
+++ modules/eks/cluster.tf
@@ -0,0 +1,28 @@
+resource "aws_eks_cluster" "main" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = var.private_subnet_ids
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
+}
+
+resource "aws_eks_node_group" "main" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = var.private_subnet_ids
+
+  scaling_config {
+    desired_size = 3
+    max_size     = 6
+    min_size     = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.ecr_read_only
+  ]
+}
