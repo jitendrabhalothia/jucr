4ac47244c54384dcb132fee3c0d1220845252297
Jitendra Singh <jitendrasingh@Priyankas-MacBook-Pro.local>
2025-05-27 08:34:21 +0200

    feat: configure internet gateway, NAT gateways, and routing tables

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -27,3 +27,59 @@ resource "aws_vpc" "main" {
       Name = "jucr-private-subnet-${count.index + 1}"
     }
   }
+  
+  resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.main.id
+    tags = {
+      Name = "jucr-igw"
+    }
+  }
+  
+  resource "aws_eip" "nat" {
+    count = 3
+    vpc   = true
+  }
+  
+  resource "aws_nat_gateway" "nat" {
+    count         = 3
+    allocation_id = aws_eip.nat[count.index].id
+    subnet_id     = aws_subnet.public[count.index].id
+    tags = {
+      Name = "jucr-nat-gateway-${count.index + 1}"
+    }
+  }
+  
+  resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.main.id
+    route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.igw.id
+    }
+    tags = {
+      Name = "jucr-public-rt"
+    }
+  }
+  
+  resource "aws_route_table_association" "public" {
+    count          = 3
+    subnet_id      = aws_subnet.public[count.index].id
+    route_table_id = aws_route_table.public.id
+  }
+  
+  resource "aws_route_table" "private" {
+    count  = 3
+    vpc_id = aws_vpc.main.id
+    route {
+      cidr_block     = "0.0.0.0/0"
+      nat_gateway_id = aws_nat_gateway.nat[count.index].id
+    }
+    tags = {
+      Name = "jucr-private-rt-${count.index + 1}"
+    }
+  }
+  
+  resource "aws_route_table_association" "private" {
+    count          = 3
+    subnet_id      = aws_subnet.private[count.index].id
+    route_table_id = aws_route_table.private[count.index].id
+  }
